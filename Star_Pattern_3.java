0e3720110601f27c952dfa24025341f8d2943c0b
Harshit Jain <harshitostwal1234@gmail.com>
2023-09-20 23:05:21 +0530

    Printing Star pattern

---
+++ Star_Pattern_3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Star_Pattern_3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int nst = n / 2 + 1;
+        int nsp = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= nsp; k++) {
+                System.out.print(" ");
+            }
+            for (int m = 1; m <= nst; m++) {
+                System.out.print("*");
+            }
+            if (i <= n / 2) {
+                nst--;
+                nsp = nsp + 2;
+            } else {
+                nst++;
+                nsp = nsp - 2;
+            }
+            System.out.println();
+        }
+    }
+}
